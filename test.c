f70dc8c470db273f5cc70b999e2c70eecb15c0fa
superuserC <he201447@students.ephec.be>
2017-02-23 22:40:04 +0100

    creating main function

--- test.c
+++ test.c
@@ -1,2 +1,6 @@
 //ceci est un code C qui ne contient qu'un commentaire...
 //ceci est une seconde ligne de commentaire...
+#include <stdio.h>
+ void main(int argc, char argv){
+    printf("hello world !"); 
+ }
